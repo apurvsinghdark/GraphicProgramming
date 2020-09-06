262076c70c35a6931f6ee9f161b42658e153f474
Apurv singh <apurvsingh9713@gmail.com>
2020-09-06 17:20:02 +0530

    Fix. NormalDir

--- fragment.glsl
+++ fragment.glsl
@@ -28,7 +28,7 @@ vec3 AmbidentLight(Material material) { return material.ambident; }
 
 vec3 DiffuseLight(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0)
 {
-	vec3 diffLightDir = normalize(vs_position - lightPos0);
+	vec3 diffLightDir = normalize(lightPos0 - vs_position);
 	float diffConst = clamp(dot(diffLightDir, vs_normal), 0, 1);
 	vec3 diffuseLight = material.diffuse * diffConst;
 	
@@ -37,9 +37,9 @@ vec3 DiffuseLight(Material material, vec3 vs_position, vec3 vs_normal, vec3 ligh
 
 vec3 SpecularLight(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0, vec3 camPos)
 {
-	vec3 specLightDir = normalize(lightPos0 - vs_position);
+	vec3 specLightDir = normalize(vs_position - lightPos0);
 	vec3 reflectionSrc = normalize(reflect(specLightDir,vs_normal)); // Light Coming From Source
-	vec3 reflectOnEye = normalize(vs_position - camPos); // reflection on eye
+	vec3 reflectOnEye = normalize(camPos - vs_position); // reflection on eye
 	float specularConst = pow(max(dot(reflectOnEye, reflectionSrc), 0), 100);
 	vec3 specularLight = material.specular * specularConst;
 	
--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 Vertex vertices[] =
 {
 	///position						//color							//texcoord					//Normal
-	glm::vec3(-0.5f,0.5f,0.0f),		glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(0.0f,1.0f),		glm::vec3(0.0f,0.0f,-1.0f),
-	glm::vec3(-0.5f,-0.5f,0.0f),	glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(0.0f,0.0f),		glm::vec3(0.0f,0.0f,-1.0f),
-	glm::vec3(0.5f,-0.5f,0.0f),		glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(1.0f,0.0f),		glm::vec3(0.0f,0.0f,-1.0f),
-	glm::vec3(0.5f, 0.5f,0.0f),		glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(1.0f,1.0f),		glm::vec3(0.0f,0.0f,-1.0f),
+	glm::vec3(-0.5f,0.5f,0.0f),		glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(0.0f,1.0f),		glm::vec3(0.0f,0.0f,1.0f),
+	glm::vec3(-0.5f,-0.5f,0.0f),	glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(0.0f,0.0f),		glm::vec3(0.0f,0.0f,1.0f),
+	glm::vec3(0.5f,-0.5f,0.0f),		glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(1.0f,0.0f),		glm::vec3(0.0f,0.0f,1.0f),
+	glm::vec3(0.5f, 0.5f,0.0f),		glm::vec3(1.0f,1.0f,1.0f),		glm::vec2(1.0f,1.0f),		glm::vec3(0.0f,0.0f,1.0f),
 																	
 };
 
